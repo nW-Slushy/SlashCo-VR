6cc3d2bbb7bca9cfe71cf445bcebb6ba6e457e4e
nW-Slushy <slushyforster@googlemail.com>
2025-06-07 23:44:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ const slasherDaten = [
     name: "SID",
     klasse: "DEMON",
     gefahrenstufe: "Considerable",
-    infos: ""
+    infos: `
+      <ul>
+        <li>Cookie Monster-Maskottchen mit großkalibriger, nachladefreier .50 BMG</li>
+        <li>Wut steigt sehr schnell; <80: isst Cookies → ruht ca. 50‑80 s ab, beruhigt sich (-8 bis -16 Wut)</li>
+        <li>Bei >60 Wut zieht er seine Waffe; bei 100 mit Waffe: kann sprinten (Chase Speed 3.9 m/s)</li>
+        <li>Sehr geduldig? Niedrigster Geduldswert → wird schnell aggressiv (Patience 25)</li>
+        <li>Lautloser Patient Walk (Sneak Speed 1 m/s), leiser und zäher Jäger</li>
+      </ul>`
   },
   {
     name: "THIRSTY",
     klasse: "DEMON",
     gefahrenstufe: "Considerable",
-    infos: ""
+    infos: `
+      <ul>
+        <li>humanoid, schwarzer Anzug, weiße Maske mit Rissen</li>
+        <li>Sehr leises Umherlaufen; reagiert hauptsächlich auf Geräusche</li>
+        <li>Trinkt Milch, wenn [ANGER] niedrig; dadurch passiv + Wut reduziert</li>
+        <li>Wut steigt schnell bei Provokation oder Nähe</li>
+        <li>Ab >70 Wut wird Thirsty aggressiv – jagt und tötet Spieler:innen</li>
+        <li>Milch beruhigt: 40–70 s bei 0 Wut, weniger bei höherem Wutlevel</li>
+        <li>Attribute: Hearing 75, Eyesight 30, Walk 1.5 m/s, Chase 4 m/s</li>
+      </ul>`
   },
   {
     name: "DOLPHINMAN",
@@ -24,8 +40,12 @@ const slasherDaten = [
     gefahrenstufe: "Considerable",
     infos: `
       <ul>
-        <li>Schnelles Fortbewegen im Wasser</li>
-        <li>Reagiert empfindlich auf Geräusche</li>
+        <li>Humanoid mit Delphinmerkmalen, schleimige Haut</li>
+        <li>Sehr schlechtes Sehvermögen (Eyesight 2), exzellentes Gehör (Hearing 90)</li>
+        <li>Nutzt Echolocation und lautes Wail, sobald Geräusche ihn reizen</li>
+        <li>Beim Jagen: Sehfähigkeit steigt auf 100</li>
+        <li>Speed: Walk 1,4 m/s, Sneak 0,8 m/s, Chase 4 m/s (+0,4 m/s bei 100 Wut)</li>
+        <li>Wut-Gewinn: angeregt durch Geräusche, passive Wutgain 1,25×, Chase-Gain 1,5×</li>
       </ul>`
   },
   {
@@ -42,7 +62,11 @@ const slasherDaten = [
     name: "THE BEAST",
     klasse: "UMBRA",
     gefahrenstufe: "Considerable",
-    infos: ""
+    infos: `
+      <ul>
+        <li>Besitzt Beißattacke</li>
+        <li>Kann von Bierfässern betäubt werden</li>
+      </ul>`
   },
   {
     name: "PRINCESS",
@@ -50,9 +74,8 @@ const slasherDaten = [
     gefahrenstufe: "Considerable",
     infos: `
       <ul>
-        <li>Reagiert auf Puppen</li>
         <li>Besitzt Beißattacke</li>
-        <li>Kann in der Nähe von Bierfässern betäubt werden</li>
+        <li>Kann von Bierfässern betäubt werden</li>
       </ul>`
   },
   {
